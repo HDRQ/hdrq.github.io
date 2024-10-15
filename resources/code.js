9ad751f94cf7cfe99682a742c25f902ffd6d250d
Nemozikr <ebhomrah@gmail.com>
2024-10-15 22:20:07 +0100

    yeah

---
+++ resources/code.js
@@ -0,0 +1,12 @@
+var a = 0;
+var b = 0;
+
+function time(a){
+    a = a + 1;
+    setTimeout(() => {}, 1000);
+    if (a > 59){
+        a = a - 60;
+        b = b + 1;
+    }
+    document.getElementById("time").innerHTML(`time: ${b}:${a}`);
+}
